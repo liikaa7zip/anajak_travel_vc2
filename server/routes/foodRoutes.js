e302dde624b55ccc2768816129767f81a2fab3c3
Sreyka <honsreyka6@gmail.com>
2025-07-31 10:03:56 +0700

    feat(User plan):create uer plan AT-134

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -7,9 +7,10 @@ router.get('/', foodController.getAllFood);
 router.get('/:id', foodController.getFoodById)
 
 // ✅ Correct way to upload image
-router.post('/', upload.single('image'), foodController.createFood);
+// router.post('/', upload.single('image'), foodController.createFood);
 // routes/foodRoutes.js
 router.get('/by-location/:locationId', foodController.getFoodsByLocation);
+
 
 
 module.exports = router;
