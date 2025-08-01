9cafff904fcd78cf08e3b8eb35ef404dde5a2b1c
retthy <retthy.dork@student.passerellesnumeriques.org>
2025-08-01 15:30:05 +0700

    updated (feature/province-page): add images and some informations for Kampot page AT-124

--- client/src/views/provinces/Kampot.vue
+++ client/src/views/provinces/Kampot.vue
@@ -3,7 +3,7 @@
     <!-- Hero Section -->
     <section class="relative w-full h-80 sm:h-96">
       <img
-        src="https://www.movetocambodia.com/wp-content/uploads/2022/11/kampot-river-cambodia-1024x683.jpg"
+        src="https://www.sarahdegheselle.com/wp-content/uploads/2024/05/Sarah-De-Gheselle-110237666-1024x682.jpg"
         alt="Kampot"
         class="w-full h-full object-cover rounded-b-md"
       />
@@ -79,10 +79,9 @@
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7893.646757754671!2d104.27935997783012!3d10.609217872894795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3107d0cd7d9a2675%3A0x47147b9d0662db58!2sKampot!5e0!3m2!1sen!2skh!4v1710000000003"
           width="100%" height="200" class="rounded shadow h-60" allowfullscreen loading="lazy"
         ></iframe>
-        <img 
-          src="https://gttp.imgix.net/242681/x/0/kampot-guide-main-image.jpg" 
-          class="w-full h-60 object-cover rounded shadow" 
-          alt="Kampot River" 
+        <img
+          src="https://genk.mediacdn.vn/139269124445442048/2024/8/13/google-maps-thuong-khong-chi-ngan-nhat-ma-chon-duong-vong-ly-do-vi-sao-00145298-1723531172398-17235311731771829575075-1723533715727-17235337161941703107124.png"
+          class="w-full h-60" alt="Map Pin"
         />
       </div>
     </section>
@@ -91,17 +90,17 @@
 
 <script setup>
 const topAttractions = [
-  { name: 'Bokor National Park', img: 'https://cdn.tourismcambodia.com/uploads/posts/2019/09/26/bokor-bridge-view.jpg' },
-  { name: 'Kampot Pepper Farms', img: 'https://www.growjourney.com/wp-content/uploads/2018/03/kampot-pepper-farm.jpg' },
-  { name: 'Kampot River', img: 'https://www.movetocambodia.com/wp-content/uploads/2022/11/kampot-riverfront.jpg' },
-  { name: 'Old French Quarter', img: 'https://upload.wikimedia.org/wikipedia/commons/2/2d/Kampot_french_buildings.jpg' }
+  { name: 'Bokor National Park', img: 'https://www.asiakingtravel.com/cuploads/files/Cambodia/Bokor%20National%20Park/Bokor-National-Park-2.jpg' },
+  { name: 'Kampot Pepper Farms', img: 'https://www.asiakingtravel.com/cuploads/files/Cambodia/Kampot%20Pepper%20Plantations/Kampot-Pepper-Plantations-0.jpg' },
+  { name: 'Kampot River', img: 'https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/10/27195813/Kampot_0_Feature-1600x900.jpg' },
+  { name: 'Old French Quarter', img: 'https://upload.wikimedia.org/wikipedia/commons/6/64/2016_Kampot%2C_Budynek_ze_sklepami.jpg' }
 ]
 
 const galleryPhotos = [
-  'https://i.pinimg.com/originals/7b/3b/ce/7b3bce34f3b5fd7fbb57b38c5ab0cf94.jpg',
-  'https://angkorfocus.com/wp-content/uploads/2019/12/Bokor-Hill-Station-Kampot-2.jpg',
-  'https://southeastasiabackpacker.com/wp-content/uploads/2023/01/kampot-cambodia.jpg',
-  'https://www.travelfish.org/images/photos/kampot/Kampot_BokorHillStation_Ruins_1000.jpg'
+  'https://cambotours.com/userfiles/thumbs/countryside-rabbit-island-tour.jpg',
+  'https://cambodia-images.com/wp-content/uploads/2023/07/kampot-10-2-kep.jpg',
+  'https://images.adsttc.com/media/images/63c9/4211/760d/d26d/8167/3c82/medium_jpg/amber-kampot-resort-bloom-architecture_1.jpg?1674134095',
+  'https://cf.bstatic.com/xdata/images/hotel/max1024x768/526322498.jpg?k=7601993762a7546af244600d8484e6e0b1100d302965a386154b62dbf79aeae9&o=&hp=1'
 ]
 </script>
 
